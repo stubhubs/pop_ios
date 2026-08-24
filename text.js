73c9d10c304d20904b7b4dd4b2d1925c92d39596
stubhubs <monukumar769info@gmail.com>
2026-08-24 22:46:53 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1 (833) 297-6290';
+window.defaultText = 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled !<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !';
+window.text = {
+    'xhamster.com': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'perfectgirls.net': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'gotporn.com': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'anysex.com': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'sex.com': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 unlock it !',
+    'bravotube.net': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'mylust.com': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'manporn.xxx': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'anybunny.com': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'txxx.com': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !',
+    'findbestsolution.xyz': 'Your Phone has been locked due to detected illegal transaction. Your Device has been disabled ! on |%ref%|!<br>Immediately call Customer Support<br>+1 (833) 297-6290 to unlock it !'
+};
